5afa28c3b1801174f9b15b63b5eec6abd4656219
Another <89122911+hexvel@users.noreply.github.com>
2024-04-02 19:51:20 +0300

    first commit

---
+++ admin-dashboard/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<div className='app'>
+			<div className='grid h-[97%] w-[97%] rounded-[2rem] bg-[var(--glass)] overflow-hidden'></div>
+		</div>
+	)
+}
+
+export default App
