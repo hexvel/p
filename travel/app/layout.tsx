2984102679ff47973beec881dd8a7f87f19ee5d6
Another <89122911+hexvel@users.noreply.github.com>
2024-04-06 22:31:24 +0300

    finish project (travel)

--- travel/app/layout.tsx
+++ travel/app/layout.tsx
@@ -1,25 +1,28 @@
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+
 import './globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-	title: 'Travel',
-	description: 'Travel UI/UX App for camping',
+  title: 'Travel',
+  description: 'Travel UI/UX App for Camping',
 }
 
 export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode
-}>) {
-	return (
-		<html lang='en'>
-			<body>
-				<Navbar />
-				<main className='relative overflow-hidden'>{children}</main>
-				<Footer />
-			</body>
-		</html>
-	)
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
 }
--- travel/components/Button.tsx
+++ travel/components/Button.tsx
@@ -1,24 +1,23 @@
-'use client'
-
-import Image from 'next/image'
+import Image from "next/image";
 
 type ButtonProps = {
-	type: 'button' | 'submit'
-	icon?: string
-	children?: React.ReactNode
-	variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text'
+  type: 'button' | 'submit';
+  title: string;
+  icon?: string;
+  variant: string;
+  full?: boolean;
 }
 
-const Button = ({ type, icon, children, variant }: ButtonProps) => {
-	return (
-		<button
-			className={`flexCenter gap-3 rounded-full border ${variant}`}
-			type='button'
-		>
-			{icon && <Image src={icon} alt={type} width={24} height={24} />}
-			<label className='bold-16 whitespace-nowrap'>{children}</label>
-		</button>
-	)
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+  return (
+    <button
+    className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+    </button>
+  )
 }
 
 export default Button
