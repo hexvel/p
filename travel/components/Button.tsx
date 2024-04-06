d3b26790db5454a4337c9cb57dbbfdcbccd7b8b7
Another <89122911+hexvel@users.noreply.github.com>
2024-04-06 19:12:49 +0300

    added navbar (travel)

---
+++ travel/components/Button.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Image from 'next/image'
+
+type ButtonProps = {
+	type: 'button' | 'submit'
+	icon?: string
+	children?: React.ReactNode
+	variant: 'btn_dark_green'
+}
+
+const Button = ({ type, icon, children, variant }: ButtonProps) => {
+	return (
+		<button
+			className={`flexCenter gap-3 rounded-full border ${variant}`}
+			type='button'
+		>
+			{icon && <Image src={icon} alt={type} width={24} height={24} />}
+			<label className='bold-16 whitespace-nowrap'>{children}</label>
+		</button>
+	)
+}
+
+export default Button
